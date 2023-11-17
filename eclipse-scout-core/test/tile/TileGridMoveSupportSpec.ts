49855d314e16cd807b7cbe83dc2e66d61d544f57
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2023-11-17 16:42:55 +0100

    TileGrid: add dnd support to move tiles

---
+++ eclipse-scout-core/test/tile/TileGridMoveSupportSpec.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+describe('TileGridMoveSupport', () => {
+  describe('drop on another tile', () => {
+    it('switches tile positions', () => {
+
+    });
+
+    it('switches grid data', () => {
+
+    });
+  });
+
+  it('cancels moving if widget is removed', () => {
+
+  });
+});
