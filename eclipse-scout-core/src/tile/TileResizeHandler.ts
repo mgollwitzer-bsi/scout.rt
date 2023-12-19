fe5eeca2a3d285baf14bd1a4554260a39fc56c8e
Claudio Guglielmo <claudio.guglielmo@bsi-software.com>
2023-12-19 23:19:33 +0100

    TileGrid: add resize support

--- eclipse-scout-core/src/tile/TileResizeHandler.ts
+++ eclipse-scout-core/src/tile/TileResizeHandler.ts
@@ -7,7 +7,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  */
-import {arrays, graphics, GridData, InitModelOf, LogicalGridLayout, Point, Rectangle, Resizable, ResizableModel, scout, Tile, TileGrid} from '..';
+import {graphics, GridData, InitModelOf, LogicalGridLayout, Point, Rectangle, Resizable, ResizableModel, scout, Tile, TileGrid} from '..';
 
 /*
  * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
@@ -30,11 +30,51 @@ export class TileResizeHandler extends Resizable {
   protected override _computeBounds(event: JQuery.MouseMoveEvent): Rectangle {
     let cell = this._findCell(this._relativeCursorPos(this.tileGrid.$container, event));
     if (!cell) {
-      return null;
+      return;
     }
-    // TODO CGU determine initialCell based on dragged edge. Bottom-right -> top-left, right -> left etc.
-    let initialCell = this._findCell(this._context.initialBounds.point());
+    let initialCell = this._findInitialCell();
+    if (scout.isOneOf(this._context.edge, 'w', 'e')) {
+      // If resized horizontally, height must not change
+      cell.y = initialCell.y;
+      cell.height = initialCell.height;
+    } else if (scout.isOneOf(this._context.edge, 'n', 's')) {
+      // If resized vertically, width must not change
+      cell.x = initialCell.x;
+      cell.width = initialCell.width;
+    }
     return initialCell.union(cell);
+  }
+
+  /**
+   * @returns the cell bounds for the cell that is on the other side of the dragged edge.
+   */
+  protected _findInitialCell() {
+    let tile = scout.widget(this.$container) as Tile;
+    let gridData = tile.gridData;
+    let row = gridData.y - 1;
+    let col = gridData.x - 1;
+    let w = gridData.w - 1;
+    let h = gridData.h - 1;
+    let layout = this.tileGrid.htmlComp.layout as LogicalGridLayout;
+    let cellBounds = layout.info.cellBounds;
+    switch (this._context.edge) {
+      case 'n':
+        return cellBounds[row + h][col].union(cellBounds[row + h][col + w]);
+      case 'ne':
+        return cellBounds[row + h][col];
+      case 'e':
+        return cellBounds[row][col].union(cellBounds[row + h][col]);
+      case 'se':
+        return cellBounds[row][col];
+      case 's':
+        return cellBounds[row][col].union(cellBounds[row][col + w]);
+      case 'sw':
+        return cellBounds[row][col + w];
+      case 'w':
+        return cellBounds[row][col + w].union(cellBounds[row + h][col + w]);
+      case 'nw':
+        return cellBounds[row + h][col + w];
+    }
   }
 
   protected _findCell(point: Point): Rectangle {
@@ -42,7 +82,7 @@ export class TileResizeHandler extends Resizable {
     for (let row = 0; row < layout.info.rows; row++) {
       for (let col = 0; col < layout.info.cols; col++) {
         if (layout.info.cellBounds[row][col].contains(point)) {
-          return layout.info.cellBounds[row][col];
+          return layout.info.cellBounds[row][col].clone();
         }
       }
     }
@@ -81,44 +121,33 @@ export class TileResizeHandler extends Resizable {
 
   protected _moveOtherTiles(resizedTile: Tile, newGridData: GridData) {
     let matrix = TileGrid.buildMatrix(this.tileGrid.tiles);
-    let movedTiles = new Set();
-
-    // Contains the heights of the grid cells at their position
-    let diffHMatrix = arrays.init(matrix.maxWidth, []);
-    for (let i = 0; i < diffHMatrix.length; i++) {
-      diffHMatrix[i] = arrays.init(matrix.maxHeight, 0);
-    }
 
     for (let x = newGridData.x; x < newGridData.x + newGridData.w; x++) {
       // DiffH is the cumulative height of all grid cells in a column that have been moved down
-      let diffH = 0;
+      let diffH = -1;
+      let movedTiles = new Set();
       for (let y = newGridData.y; y <= matrix.maxHeight; y++) {
         let tile = matrix[x][y];
         if (resizedTile === tile) {
-          continue;
-        }
-        let gridData = new GridData(tile.gridDataHints);
-        if (movedTiles.has(tile)) {
           continue;
         }
         if (tile.hasCssClass('item-summary-designer-placeholder')) {
           // TODO CGU make customizable to not use item-summary class
           continue;
         }
-        if (diffH === 0 && !newGridData.toRectangle().intersects(gridData.toRectangle())) {
+        let gridData = new GridData(tile.gridDataHints);
+        if (diffH === -1 && !newGridData.toRectangle().intersects(gridData.toRectangle())) {
+          continue;
+        }
+        diffH++;
+        if (movedTiles.has(tile)) {
           continue;
         }
         let newY = newGridData.y + newGridData.h + diffH;
         if (gridData.y === newY) {
           continue;
         }
-        // If a tile spans to columns, diffH of the second column needs to be increased by h as well
-        let colSpan = gridData.w - (x - gridData.x);
-        for (let i = x; i < x + colSpan; i++) {
-          // TODO CGU this does not work, maybe just create matrix with grid.h for each cell beforehand
-          diffHMatrix[i][y] += gridData.h;
-        }
-        diffH += diffHMatrix[x][y];
+        // TODO CGU large tiles in columns outside of the range may be moved and overlap other tiles
         gridData.y = newY;
         movedTiles.add(tile);
         tile.setGridDataHints(gridData);
